0149ac796f61e812de2ecec074b39ca763109efa
Bob Farrell <git@bob.frl>
2024-11-03 12:56:31 +0000

    Closes #34 Extend zmplValue to support error unions
    Evaluate the value as a Zmpl Value when no error encountered, otherwise
    return the error.

--- src/zmpl/Data.zig
+++ src/zmpl/Data.zig
@@ -1572,6 +1572,10 @@ fn zmplValue(value: anytype, alloc: std.mem.Allocator) !*Value {
                 break :blk Value{ .Null = NullType{ .allocator = alloc } };
             }
         },
+        .error_union => return if (value) |capture|
+            zmplValue(capture, alloc)
+        else |err|
+            err,
         .@"struct" => try structToValue(value, alloc),
         else => @compileError("Unsupported type: " ++ @typeName(@TypeOf(value))),
     };
