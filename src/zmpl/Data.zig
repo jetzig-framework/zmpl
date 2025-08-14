5dabe4edf85af2415412e6e1ee1c9905b9c361dc
Ben Jordan <ben@poecoh.com>
2025-08-13 21:02:22 -0400

    removing commented out lines

--- src/zmpl/Data.zig
+++ src/zmpl/Data.zig
@@ -764,9 +764,7 @@ pub fn toJsonOptions(self: *Data, comptime options: ToJsonOptions) ![]const u8 {
 pub fn parseJsonSlice(self: *Data, json: []const u8) !*Value {
     const alloc = self.allocator;
     var json_stream: std.io.Reader = .fixed(json);
-    //var json_stream = std.io.fixedBufferStream(json);
     var reader: std.json.Reader = .init(alloc, &json_stream);
-    //var reader = std.json.reader(alloc, json_stream.reader());
     var container_stack = std.ArrayList(*Value).init(alloc);
     var current_container: ?*Value = null;
     var current_key: ?[]const u8 = null;
